10d6570879770e45d994df663d353338f1161430
gununu <gununu@users.noreply.github.com>
2014-03-12 01:44:35 +0900

    Change implimentation

--- test.hpp
+++ test.hpp
@@ -6,7 +6,6 @@ class implement; //declaration only
 class sample {
     public:
     sample(); //need separation of constructor when without pimpl_noinit argument.
-    ~sample(); //need separation of destructor.
 
     gununu::pimpl<implement> pvalue;
 
