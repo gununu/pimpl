46a9cb62919a942825b59c35b6a897c8fe2fee2e
gununu <gununu@users.noreply.github.com>
2014-03-12 01:47:22 +0900

    Change implimentation

--- test.cpp
+++ test.cpp
@@ -1,15 +1,30 @@
 #include "test.hpp"
+#include <iostream>
 
 //need definition before used by sample class.
 class implement {
     public:
+    implement():val(0){}
+    implement(int v):val(v){}
     void method(){}
+    int val;
 };
 
 
 sample::sample(){}
-sample::~sample(){}
 
 void sample::func() {
     pvalue->method();
+
+
+    //test
+    (*pvalue).method();
+    pvalue.get();
+
+    sample s;
+    swap(pvalue, s.pvalue);
+    pvalue = gununu::make_pimpl<implement>();
+    if (pvalue) {}
+    pvalue = gununu::pimpl<implement>(implement(3));
+    std::cout << pvalue->val;
 }
